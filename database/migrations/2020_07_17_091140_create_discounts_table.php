967286b61ff2680daa5e818d29379895bdcf13dc
Atem Daniel <manday.daniel@gmail.com>
2020-08-03 23:43:07 +0300

    Add discounts_table migration file

---
+++ database/migrations/2020_07_17_091140_create_discounts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('rate');
+            $table->string('rate_type');
+
+            $table->integer('shop_id')->unsigned();
+                $table->foreign('shop_id')
+                    ->references('id')
+                    ->on('shops')
+                    ->onDelete('cascade');
+                    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
