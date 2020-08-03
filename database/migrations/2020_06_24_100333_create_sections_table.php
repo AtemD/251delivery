2e2ca20edbb19bf7d789a311f85de16b87736802
Atem Daniel <manday.daniel@gmail.com>
2020-08-03 22:47:47 +0300

    Add sections_table migration file

---
+++ database/migrations/2020_06_24_100333_create_sections_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+
+            $table->integer('shop_id')->unsigned();
+                $table->foreign('shop_id')
+                    ->references('id')
+                    ->on('shops')
+                    ->onDelete('cascade');
+
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+}
