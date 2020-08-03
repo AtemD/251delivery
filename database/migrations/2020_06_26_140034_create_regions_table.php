51ced3c92acca2b6ed87c45481be90dca2b4d707
Atem Daniel <manday.daniel@gmail.com>
2020-08-03 22:54:30 +0300

    Add regions_table migration file

---
+++ database/migrations/2020_06_26_140034_create_regions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->string('name');
+            $table->string('abbreviation');
+
+            $table->smallInteger('country_id')->unsigned();
+            $table->foreign('country_id')
+                ->references('id')
+                ->on('countries')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
