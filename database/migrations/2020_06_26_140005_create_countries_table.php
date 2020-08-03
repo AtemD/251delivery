a7c4380dc279a22d4626998e0ec447d0e3d266be
Atem Daniel <manday.daniel@gmail.com>
2020-08-03 22:52:51 +0300

    Add countries_table migration file

---
+++ database/migrations/2020_06_26_140005_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name');
+            $table->string('abbreviation');
+            $table->string('currency_name');
+            $table->string('currency_abbreviation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
