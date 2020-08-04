9ee29f40d82084f93cf57f536c6b997604ffc936
Atem Daniel <manday.daniel@gmail.com>
2020-08-04 13:01:37 +0300

    Add Models: Product

---
+++ app/Models/Product.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'status', 'shop_id', 'section_id', 'image', 'description', 'base_price'
+    ];
+
+    // Account Statuses
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $appends = ['price', 'image_path'];
+
+    public function shop()
+    {
+        return $this->belongsTo('App\Shop');
+    }
+
+    public function section()
+    {
+        return $this->belongsTo('App\Section');
+    }
+
+    public function taxes()
+    {
+        return $this->belongsToMany('App\Tax', 'product_has_tax', 'product_id', 'tax_id');
+    }
+
+    public function discounts()
+    {
+        return $this->belongsToMany('App\Discount', 'product_has_discount', 'product_id', 'discount_id');
+    }
+
+    public function getPriceAttribute() :float
+    {
+        return $this->base_price / 100;
+    }
+
+    public function getImagePathAttribute() :string
+    {
+        return "/uploads/shops/products/{$this->image}";
+    }
+}
