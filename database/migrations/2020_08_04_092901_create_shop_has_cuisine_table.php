27ec088dd036680f31b282137901af3f59b7d0aa
Atem Daniel <manday.daniel@gmail.com>
2020-08-04 12:33:26 +0300

    Add shop_has_cuisine_table migration file

---
+++ database/migrations/2020_08_04_092901_create_shop_has_cuisine_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopHasCuisineTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_has_cuisine', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('shop_id')->unsigned();
+                $table->foreign('shop_id')
+                    ->references('id')
+                    ->on('shops')
+                    ->onDelete('cascade');
+
+            $table->smallInteger('cuisine_id')->unsigned();
+                $table->foreign('cuisine_id')
+                    ->references('id')
+                    ->on('cuisines')
+                    ->onDelete('cascade');
+
+            $table->timestamps();
+
+            $table->unique(['shop_id', 'cuisine_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_has_cuisine');
+    }
+}
