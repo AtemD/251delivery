6886e86d1b34065ac8b3b69affcae5a32d45ee5c
Atem Daniel <manday.daniel@gmail.com>
2020-08-04 14:38:34 +0300

    Add Model: Tax

---
+++ app/Models/Tax.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tax extends Model
+{
+    // Rate types
+    const PERCENTAGE_TAX = 'percentage';
+
+    // tax Statuses
+    const ENABLED_TAX = 'enabled';
+    const DISABLED_TAX = 'disabled';
+
+    public function shop()
+    {
+        return $this->belongsTo('App\Shop');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Product', 'product_has_tax', 'tax_id', 'product_id');
+    }
+}
