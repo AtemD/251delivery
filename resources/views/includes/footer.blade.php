5c35bde42c8838cbfbcef9fb1825568c7dd2ecd8
Atem Daniel <manday.daniel@gmail.com>
2020-08-05 15:05:58 +0300

    Initial laravel views commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="bd-footer text-muted">
+	<div class="container">
+		<div class="row">
+			<div class="col text-center">
+				<h2 class="font-weight-light text-capitalize">{{ config('app.name') }}</h2>
+				<p class="font-weight-light font-italic mb-3">Fast and Easy Delivery!</p>
+				<div class="py-2">
+					<a href="#"></a>
+				</div>
+				<p class="py-4 m-0">&copy; Copyright - Daniel Atem</p>
+			</div>
+		</div>
+	</div>
+</footer>
