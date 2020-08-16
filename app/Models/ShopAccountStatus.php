366f77a67fba140dec646af98a6df331ba093ee0
Atem Daniel <manday.daniel@gmail.com>
2020-08-16 17:08:41 +0300

    Delete irrelevant comment on ShopAccountStatus Model

--- app/Models/ShopAccountStatus.php
+++ app/Models/ShopAccountStatus.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShopAccountStatus extends Model
 {
-    /**
-     * Get the locations for the city
-     */
+
     public function shops()
     {
         return $this->hasMany('App\Models\Shop');
