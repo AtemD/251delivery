0640afaca961d63d78dae64481be13aae07d34b1
Atem Daniel <manday.daniel@gmail.com>
2020-08-21 11:58:24 +0300

    modify order statuses table seeder

--- database/seeds/OrderStatusesTableSeeder.php
+++ database/seeds/OrderStatusesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Order;
+use App\Models\OrderStatus;
 use Illuminate\Database\Seeder;
 
 class OrderStatusesTableSeeder extends Seeder
@@ -13,40 +14,40 @@ class OrderStatusesTableSeeder extends Seeder
     public function run()
     {
         // Name, description, color
-        factory(App\Models\OrderStatus::class)->create([
+        factory(OrderStatus::class)->create([
             'name' => Order::PENDING_ORDER,
             'description' => 'a new order',
-            'color' => 'primary'
+            'color' => 'info'
         ]);
-        factory(App\Models\OrderStatus::class)->create([
+        factory(OrderStatus::class)->create([
             'name' => Order::APPROVED_ORDER,
             'description' => 'an accepted order',
             'color' => 'primary'
         ]);
-        factory(App\Models\OrderStatus::class)->create([
+        factory(OrderStatus::class)->create([
             'name' => Order::READY_ORDER,
             'description' => 'a ready to be delivered order',
-            'color' => 'primary'
+            'color' => 'success'
         ]);
-        factory(App\Models\OrderStatus::class)->create([
+        factory(OrderStatus::class)->create([
             'name' => Order::DELIVERING_ORDER,
             'description' => 'an order on the way to the customer',
-            'color' => 'primary'
+            'color' => 'info'
         ]);
-        factory(App\Models\OrderStatus::class)->create([
+        factory(OrderStatus::class)->create([
             'name' => Order::COMPLETED_ORDER,
             'description' => 'an order that the customer has received and is satisfied with it',
-            'color' => 'primary'
+            'color' => 'success'
         ]);
-        factory(App\Models\OrderStatus::class)->create([
+        factory(OrderStatus::class)->create([
             'name' => Order::REJECTED_ORDER,
             'description' => 'an order rejected by the shop due to some reason',
-            'color' => 'primary'
+            'color' => 'warning'
         ]);
-        factory(App\Models\OrderStatus::class)->create([
+        factory(OrderStatus::class)->create([
             'name' => Order::CANCELLED_ORDER,
             'description' => 'an order cancelled by the customer before it is approved',
-            'color' => 'primary'
+            'color' => 'danger'
         ]);
     }
 }
