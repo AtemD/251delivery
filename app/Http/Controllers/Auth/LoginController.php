ed2262c518eac3d6bec78e3c5a12d8616447120a
Atem Daniel <manday.daniel@gmail.com>
2020-09-08 12:49:37 +0300

    Modify LoginController to redirect users to different routes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -36,5 +37,23 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        if($user->hasPermissionTo('access retailer dashboard')){
+            return redirect()->route('retailer.home');
+        } else if($user->hasPermissionTo('access administrator dashboard')){
+            return redirect()->route('company.home');
+        } else {
+            return redirect()->route('home');
+        }
     }
 }
