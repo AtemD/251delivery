484562f0b42bff8e491c52f97ee298fb4d521976
Atem Daniel <manday.daniel@gmail.com>
2020-10-22 18:21:48 +0300

    Fix add correct buyers account route

--- resources/views/dashboard/buyer/includes/main-sidebar.blade.php
+++ resources/views/dashboard/buyer/includes/main-sidebar.blade.php
@@ -73,7 +73,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="{{ route('buyers.settings.accounts.index') }}" class="nav-link">
+                    <a href="{{ route('buyers.accounts.index') }}" class="nav-link">
                       <i class="fas fa-circle nav-icon"></i>
                       <p>Account Settings</p>
                     </a>
