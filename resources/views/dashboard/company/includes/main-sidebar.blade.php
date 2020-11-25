c4b913b16567dcc52ad844507f0c3f7fb4e12eff
Atem Daniel <manday.daniel@gmail.com>
2020-11-25 09:48:37 +0300

    Fix: broken link

--- resources/views/dashboard/company/includes/main-sidebar.blade.php
+++ resources/views/dashboard/company/includes/main-sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-          <img src="/uploads/images/project.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <a href="/" class="brand-link">
+          <img src="/uploads/images/project.png" alt="251Delivery Logo" class="brand-image img-circle elevation-3"
                style="opacity: .8">
           <span class="brand-text font-weight-light">251Delivery</span>
         </a>
