6a10f73daeb62b93b3d4b395adeafc28a758d639
Atem Daniel <manday.daniel@gmail.com>
2020-11-25 09:47:35 +0300

    Fix: Add appropriate routes

--- resources/views/dashboard/buyer/includes/main-sidebar.blade.php
+++ resources/views/dashboard/buyer/includes/main-sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="/" class="brand-link">
           <img src="/uploads/images/project.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                style="opacity: .8">
           <span class="brand-text font-weight-light">251Delivery</span>
@@ -14,7 +14,7 @@
               <img src="/uploads/images/customer-support.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-              <a href="#" class="d-block">John Doe</a>
+              <a href="{{ route('buyers.accounts.index') }}" class="d-block">{{ auth()->user()->full_name }}</a>
             </div>
           </div>
 
@@ -24,7 +24,7 @@
               <!-- Add icons to the links using the .nav-icon class
                    with font-awesome or any other icon font library -->
               <li class="nav-item">
-                <a href="{{ route('buyers.home') }}" class="nav-link">
+                <a href="{{ route('buyers.home') }}" class="nav-link {{ (request()->routeIs('buyers.home')) ? 'active' : '' }}">
                   <i class="nav-icon fas fa-tachometer-alt"></i>
                   <p>
                     Home
