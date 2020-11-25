e976962acafbba1a68f0e7b3ac6a8638bc7c4032
Atem Daniel <manday.daniel@gmail.com>
2020-11-25 09:50:09 +0300

    Feat n Fix: add logout to sidebar and fix broken home link

--- resources/views/dashboard/retailer/includes/main-sidebar.blade.php
+++ resources/views/dashboard/retailer/includes/main-sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="/" class="brand-link">
           <img src="/uploads/images/project.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                style="opacity: .8">
           <span class="brand-text font-weight-light">251Delivery</span>
@@ -110,6 +110,19 @@
                   </li>
                 </ul>
               </li>
+
+              <li class="nav-item">
+                <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                  <i class="nav-icon fas fa-sign-out-alt"></i>
+                  <p>
+                    {{ __('Logout') }}
+                  </p>
+                </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                  </form>
+              </li>
+              
             </ul>
           </nav>
           <!-- /.sidebar-menu -->
