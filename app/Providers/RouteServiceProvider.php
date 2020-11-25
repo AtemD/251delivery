6a13974a330d67e228756da7d7779ce0a5ec2551
Atem Daniel <manday.daniel@gmail.com>
2020-11-25 09:43:42 +0300

    Fix: Add home route constant

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard/buyers/home';
+    public const HOME = '/dashboard/buyer/home';
 
     /**
      * Define your route model bindings, pattern filters, etc.
