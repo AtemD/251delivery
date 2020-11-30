ce297895ae0b55f7ffb702bd28ce7e5802c00615
Atem Daniel <manday.daniel@gmail.com>
2020-11-30 14:26:04 +0300

    Add orders_has_products table

---
+++ database/migrations/2020_07_16_131029_create_order_has_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderHasProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_has_products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->foreign('order_id')
+                ->references('id')
+                ->on('orders')
+                ->onDelete('cascade');
+
+            $table->integer('product_id')->unsigned();
+                $table->foreign('product_id')
+                    ->references('id')
+                    ->on('products')
+                    ->onDelete('cascade');
+
+            $table->tinyInteger('quantity')->unsigned();
+            $table->integer('amount')->unsigned();
+            $table->string('special_request')->nullable(); // special instructions to the shop or restaurant
+            $table->timestamps();
+
+            $table->unique(['order_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_has_products');
+    }
+}
